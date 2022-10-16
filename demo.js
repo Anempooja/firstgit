81199099784d06e5a18640fabcb4926a77402614
Anempooja <anempooja@gmail.com>
2022-10-16 15:54:56 +0530

    2ndtask

--- demo.js
+++ demo.js
@@ -1,1 +1,5 @@
-console.log('welcome')
+var headerTitle=document.getElementById('header-title');
+console.log(headerTitle.innerText);
+//headerTitle.textContent="hello"
+//headerTitle.innerText="good boy"
+headerTitle.style="color:blue"
