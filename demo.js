cbe92e28c84b59786887b24cf4a1f5bdac41c821
Anempooja <anempooja@gmail.com>
2022-10-17 19:29:53 +0530

    task6

--- demo.js
+++ demo.js
@@ -10,9 +10,24 @@ console.log(headerTitle.innerText);
 //}
 
 //items[2].style.backgroundColor='yellow'
-var li=document.getElementsByTagName('li');
-li[1].textContent='Hello'
-li[2].style.fontWeight='bold'
-li[3].style.backgroundColor='#f3f3f3'
-li[0].style='color:blue'
-li[4].textContent='item5'
+//var li=document.getElementsByTagName('li');
+//li[1].textContent='Hello'
+//li[2].style.fontWeight='bold'
+//li[3].style.backgroundColor='#f3f3f3'
+//li[0].style='color:blue'
+//li[4].textContent='item5'
+var x=document.querySelector('#main-header');
+x.style.borderBottom='solid 5px yellow'
+var input=document.querySelector('input[type="submit"]');
+input.value="OK"
+var items=document.querySelector('.list-group-item');
+items.style.color='pink'
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.fontWeight='bold'
+//secondItem.style.backgroundColor='green'
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.color='transparent';
+var odd= document.querySelectorAll('li:nth-child(odd)');
+for (var i=0; i<odd.length;i++){
+    odd[i].style.backgroundColor='green'
+}
