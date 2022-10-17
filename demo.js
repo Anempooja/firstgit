7f1b9ef935b4d2c6c8bbe6d921a8ab9d477ae7a7
Anempooja <anempooja@gmail.com>
2022-10-17 18:11:50 +0530

    task5

--- demo.js
+++ demo.js
@@ -2,11 +2,17 @@ var headerTitle=document.getElementById('header-title');
 console.log(headerTitle.innerText);
 //headerTitle.textContent="hello"
 //headerTitle.innerText="good boy"
-var items= document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[1])
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
-}
+//var items= document.getElementsByClassName('list-group-item')
+//console.log(items)
+//console.log(items[1])
+//for(let i=0;i<items.length;i++){
+//    items[i].style.fontWeight='bold'
+//}
 
-items[2].style.backgroundColor='yellow'
+//items[2].style.backgroundColor='yellow'
+var li=document.getElementsByTagName('li');
+li[1].textContent='Hello'
+li[2].style.fontWeight='bold'
+li[3].style.backgroundColor='#f3f3f3'
+li[0].style='color:blue'
+li[4].textContent='item5'
