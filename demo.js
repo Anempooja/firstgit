bdad45e9b010dde4c49816723ad5db33766c4aae
Anempooja <anempooja@gmail.com>
2022-10-18 18:46:06 +0530

    task8

--- demo.js
+++ demo.js
@@ -1,33 +1,27 @@
-var headerTitle=document.getElementById('header-title');
-console.log(headerTitle.innerText);
-//headerTitle.textContent="hello"
-//headerTitle.innerText="good boy"
-//var items= document.getElementsByClassName('list-group-item')
-//console.log(items)
-//console.log(items[1])
-//for(let i=0;i<items.length;i++){
-//    items[i].style.fontWeight='bold'
-//}
+var itemList=document.querySelector('#items');
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='orange';
+itemList.lastElementChild.style.backgroundColor='#f4f4f4';
+console.log(itemList.lastChild);
+itemList.firstElementChild.style.backgroundColor='yellow';
+console.log(itemList.firstChild);
+itemList.firstElementChild.style.backgroundColor='pink';
+itemList.firstElementChild.textContent='Hello'
+console.log(itemList.nextSibling);
+console.log(itemList.nextElementSibling);
+console.log(itemList.previousSibling);
+itemList.previousElementSibling.style.backgroundColor='#f4f4f4'
+var newDiv= document.createElement('div');
+newDiv.className='hello'
+newDiv.id='hello1'
+newDiv.setAttribute('title','Hello Div');
+var newDivText=document.createTextNode('Hello world');
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize='50px';
+container.insertBefore(newDiv,h1);
+itemList.firstElementChild.appendChild(newDiv)
 
-//items[2].style.backgroundColor='yellow'
-//var li=document.getElementsByTagName('li');
-//li[1].textContent='Hello'
-//li[2].style.fontWeight='bold'
-//li[3].style.backgroundColor='#f3f3f3'
-//li[0].style='color:blue'
-//li[4].textContent='item5'
-var x=document.querySelector('#main-header');
-x.style.borderBottom='solid 5px yellow'
-var input=document.querySelector('input[type="submit"]');
-input.value="OK"
-var items=document.querySelector('.list-group-item');
-items.style.color='pink'
-var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.fontWeight='bold'
-//secondItem.style.backgroundColor='green'
-var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.color='transparent';
-var odd= document.querySelectorAll('li:nth-child(odd)');
-for (var i=0; i<odd.length;i++){
-    odd[i].style.backgroundColor='green'
-}
+
