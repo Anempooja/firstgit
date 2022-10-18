bfe82b5ecce8765af20a1444cac3066a7d3c7b29
Anempooja <anempooja@gmail.com>
2022-10-18 20:49:51 +0530

    task 8 completed

--- demo.js
+++ demo.js
@@ -1,27 +1,27 @@
-var itemList=document.querySelector('#items');
-console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor='orange';
-itemList.lastElementChild.style.backgroundColor='#f4f4f4';
-console.log(itemList.lastChild);
-itemList.firstElementChild.style.backgroundColor='yellow';
-console.log(itemList.firstChild);
-itemList.firstElementChild.style.backgroundColor='pink';
-itemList.firstElementChild.textContent='Hello'
-console.log(itemList.nextSibling);
-console.log(itemList.nextElementSibling);
-console.log(itemList.previousSibling);
-itemList.previousElementSibling.style.backgroundColor='#f4f4f4'
-var newDiv= document.createElement('div');
-newDiv.className='hello'
-newDiv.id='hello1'
-newDiv.setAttribute('title','Hello Div');
-var newDivText=document.createTextNode('Hello world');
-newDiv.appendChild(newDivText);
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
-console.log(newDiv);
-newDiv.style.fontSize='50px';
-container.insertBefore(newDiv,h1);
-itemList.firstElementChild.appendChild(newDiv)
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
+function addItem(e){
+  e.preventDefault();
+  var newItem = document.getElementById('item').value;
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(newItem));
+  var deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteBtn);
+  itemList.appendChild(li);
+}
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
