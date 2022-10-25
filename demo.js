b68fd0a0a162e9597b0dab713f39d4fe5ab69378
Anempooja <anempooja@gmail.com>
2022-10-25 16:55:17 +0530

    advanced Javascript task9

--- demo.js
+++ demo.js
@@ -1,50 +1,14 @@
-var form = document.getElementById('my-form');
-var itemList = document.getElementById('users');
-
-
-
-form.addEventListener('submit', addItem);
-itemList.addEventListener('click',removeItem);
-
-
-function addItem(e){
-  e.preventDefault();
-  let myObj={
-    Name : document.getElementById('name').value,
-    Email : document.getElementById('email').value
-  };
-
-
- 
-  if(myObj.Email!==null){
-  delete(localStorage.getItem(myObj.Email))
-  }
-
-  localStorage.setItem(myObj.Email,myObj.Name+' '+myObj.Email);
-  
-  var li = document.createElement('li');
-  li.className = 'item';
-  li.appendChild(document.createTextNode(myObj.Name+' '+myObj.Email));
-  var editBtn=document.createElement('button')
-  editBtn.appendChild(document.createTextNode('Edit'));
-  editBtn.className = ' btn-danger btn-sm float-right edit';
-  li.appendChild(editBtn);
-  itemList.appendChild(li)
-  var deleteBtn = document.createElement('button');
-  deleteBtn.className = ' btn-danger btn-sm float-right delete';
-  deleteBtn.appendChild(document.createTextNode('Delete'));
-  li.appendChild(deleteBtn);
-  itemList.appendChild(li);
-  
-
-}
-function removeItem(e){
-  
-  if(e.target.classList.contains('delete')){
-   {
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
+let multiply=function(x,y){
+    console.log(x*y)
+}    
+let multiplyTwo=multiply.bind(this,2)
+multiplyTwo(5)
+let multiplyThree=multiply.bind(this,3)
+multiplyTwo(15)
+let sum=function(a){
+    return function(b){
+        console.log(a+b)
     }
-  }
-
-    }
+}    
+let add=sum(5)
+add(10)
