243cb903ed7e0d414f98b80ef0076b6c6119241b
Anempooja <anempooja@gmail.com>
2022-11-25 18:39:23 +0530

    promise all

--- promises.js
+++ promises.js
@@ -12,7 +12,8 @@ function getPosts(){
         for (var i=0;i<posts.length;i++){
             output+=`<li>${posts[i].title}-last updated ${(new Date().getTime() - posts[i].createdAt)/1000}- seconds ago</li>`
             
-            
+                
+             
         };
         document.body.innerHTML=output;
     },1000);
@@ -26,15 +27,19 @@ function createPost(post){
         
         const error=false;
         if(!error){
+            console.log('Before creating post4 user last active time:',new Date())
             resolve();
         }
         else{
             reject('Error:something wemt wrong');
         }
-
+        console.log(posts)
+        
     },1000);
 });
+
 }
+
 function deletePost(){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
@@ -48,22 +53,29 @@ function deletePost(){
     })
 }
 
+function create4thPost(post,callback){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            posts.push({...post,createdAt:new Date().getTime()});
+           
+        
+        const error=false;
+        if(!error){
+            
+            resolve();
+        }
+        else{
+            reject('Error:something wemt wrong');
+        }
+        console.log('after creating post 4 user last activity time',new Date().getTime())
+    },1000);
+});
+}
 
-createPost({title: 'Post Three',body:'This is post three'})
-    .then(()=>{
-        getPosts()
-        deletePost().then(()=>{
-            getPosts();
-            deletePost().then(()=>{
-                getPosts();
-                deletePost().then(()=>{
-                    getPosts();
-                    deletePost().then(()=>{})
-                    .catch((err)=>{
-                        //console.log('Inside catch block:',err)
-                    })
-                }).catch((err)=>{})
-            }).catch((err)=>{})
-        })
-    })
-    .catch(err => console.log(err))
+createPost({title:'Post Three', body:'This is post three'});
+create4thPost({title:'Post Four', body:'This is post four'});
+
+Promise.all([createPost,create4thPost])
+.then(getPosts)
+.catch(err=> console.log(err))
+
