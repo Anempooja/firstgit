5a084add42b4d642e0638bb3f960513d642c3b92
Anempooja <anempooja@gmail.com>
2022-12-20 19:06:05 +0530

    server responses

--- practise.js
+++ practise.js
@@ -1,10 +1,11 @@
 const http = require('http')
 const server=http.createServer((req,res)=>{
-    res.write('pooja')
+    console.log(req)
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write(`<head><title>Pooja</title></head>`)
+    res.write(`<body><h1>welcome to my node js project<h1></body>`)
     res.end()
-    
 });      
-server.listen(4000,()=>{
-    console.log('pooja')
-})
+server.listen(4000)
     
