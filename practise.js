48db31b85d939b5bff0fc82e4d9e3708aed2339e
Anempooja <anempooja@gmail.com>
2022-12-20 20:13:58 +0530

    server form

--- practise.js
+++ practise.js
@@ -1,11 +1,25 @@
+const fs = require('fs');
 const http = require('http')
 const server=http.createServer((req,res)=>{
-    console.log(req)
+    const url=req.url
+    const method=req.method
+    if(url==='/'){
+        res.write('<html>')
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt','DUMMY')
+        res.statusCode=302;
+        res.setHeader('Location', '/')
+        return res.end()
+    }
     res.setHeader('Content-Type','text/html')
     res.write('<html>')
-    res.write(`<head><title>Pooja</title></head>`)
-    res.write(`<body><h1>welcome ${req.url}<h1></body>`)
-    res.end()
+    res.write('<head><title> server</title></head>')
+    res.write('<body><h1> Hello </h1></body>')
 });      
 server.listen(4000)
     
