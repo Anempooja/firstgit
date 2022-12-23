e512c2a6f89af835ea13ddfa75fea993109855eb
Anempooja <anempooja@gmail.com>
2022-12-23 12:17:20 +0530

    express res.send

--- practise.js
+++ practise.js
@@ -1,6 +1,15 @@
 
 const http = require('http')
-const routes=require('./routes')
-const server=http.createServer(routes.handler)     
-server.listen(4000)
+const express=require('express')
+const app =express()
+app.use((req,res,next)=>{
+    console.log("in the middleware 1")
+    next()
+})
+app.use((req,res,next)=>{
+    console.log("in the middleware 2")
+    res.send('<h1> Hello </h1>')
+})
+
+app.listen(3000)
     
