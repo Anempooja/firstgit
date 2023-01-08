739d7e7285d41c9a0aabe24d5976457cb009eaa4
Anempooja <anempooja@gmail.com>
2023-01-08 15:29:02 +0530

    cart with many to many relation

--- controllers/shop.js
+++ controllers/shop.js
@@ -46,19 +46,72 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  req.user.getCart()
+  .then((cart)=>{
+    return cart.getProducts()
+    .then(products=>{
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products:products
+    })
+  })
+  .catch(err=>(console.log(err)))
+  
+  }).catch(err=>console.log(err))
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
+  let fetchedCart;
+  let newQuantity=1;
+  req.user.getCart()
+  .then(cart=>{
+    fetchedCart=cart
+    return cart.getProducts({where:{id:prodId}})
+  })
+  .then(products=>{
+    let product;
+    if(products.length>0){
+      product=products[0]
+    }
+    
+    if(product){
+      const oldQuantity=Product.CartItem.quantity
+      oldQuantity=oldQuantity+newQuantity
+      return product
+    }
+    return Product.findByPk(prodId)
+   
+  })
+  .then(product=>{
+    console.log(newQuantity)
+    return fetchedCart.addProduct(product,{through:{quantity:newQuantity}})
+  })
+  .then(()=>{
+    res.redirect('/cart')
+  })
+  .catch(err=>console.log(err))
+  Product.findByPk(prodId, product => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
 };
+exports.postCartDeleteProduct=(req,res,next)=>{
+  const prodId=req.body.productId;
+  req.user.getCart()
+  .then(cart=>{
+    
+    return cart.getProducts({where:{id:prodId}})
+    .then(products=>{
+      const product=products[0]
+      return product.cartItem.destroy()
+    })
+    .then(result=>{
+      res.redirect('/cart')
+    })
+  }).catch(err=>console.log(err))
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
